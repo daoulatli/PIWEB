bf18c3f0b18160fcad8c847b75b32fa866afe504
daoulatli med moez <medmoez.daoulatli@esprit.tn>
2017-04-07 02:58:28 +0200

    first commit
    Mr Bricolage Project

---
+++ seed/product-seeder.js
@@ -0,0 +1,54 @@
+var Product = require('../models/product');
+var mongoose = require('mongoose');
+
+
+
+mongoose.connect('localhost:27017/mbDB');
+
+
+var products = [
+    new Product({
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: '400x500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
+        price: 10
+    }),
+     new Product({
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: '400x500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
+        price: 20
+    }),
+     new Product({
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: '400x500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
+        price: 15
+    }),
+     new Product({
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: '400x500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
+        price: 40
+    }),
+      new Product({
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: '400x500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
+        price: 50
+    })
+];
+var done = 0;
+for (i = 0; i < products.length; i++) {
+    products[i].save(function (err, result) {
+        done++;
+        if (done === products.length) {
+            exit();
+        }
+
+    });
+}
+
+function exit() {
+    mongoose.disconnect();
+}
